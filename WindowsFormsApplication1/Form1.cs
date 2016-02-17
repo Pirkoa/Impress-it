9cd1ad962d4163125ad9193d684abf773eab36f7
Cyril DUSSERT <cyril.dussert@etu.univ-savoie.fr>
2016-02-17 21:12:05 +0100

    Adding files
    v1.0

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private string stringToPrint;
+        private String path;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadFile()
+        {
+            string docName = "TEMPORARY.txt";
+            string docPath = @"C:\Users\Dussert Cyril\Desktop";
+
+            if (!Directory.Exists(docPath))
+                Directory.CreateDirectory(docPath);
+            path = Path.Combine(docPath, docName);
+            File.WriteAllText(path, textBox1.Text);
+
+            docToPrint.DocumentName = "Programme CNC";
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                stringToPrint = reader.ReadToEnd();
+            }
+        }
+
+        private void docToPrint_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            int charactersOnPage = 0;
+            int linesPerPage = 0;
+
+            // Sets the value of charactersOnPage to the number of characters 
+            // of stringToPrint that will fit within the bounds of the page.
+            e.Graphics.MeasureString(stringToPrint, this.Font,
+                e.MarginBounds.Size, StringFormat.GenericTypographic,
+                out charactersOnPage, out linesPerPage);
+
+            // Draws the string within the bounds of the page
+            e.Graphics.DrawString(stringToPrint, this.Font, Brushes.Black,
+                e.MarginBounds, StringFormat.GenericTypographic);
+
+            // Remove the portion of the string that has been printed.
+            stringToPrint = stringToPrint.Substring(charactersOnPage);
+
+            // Check to see if more pages are to be printed.
+            e.HasMorePages = (stringToPrint.Length > 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ReadFile();
+                docToPrint.Print();
+                File.Delete(path);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
